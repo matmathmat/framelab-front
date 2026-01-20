630140644bfae1755b73d2bd1dbf5e635f19787f
matmathmat <232357120+matmathmat@users.noreply.github.com>
2026-01-20 15:33:08 +0100

    Logique de la page connexion terminé

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,16 +1,60 @@
 <script setup>
+function setCookie(name, value, days) {
+    var expires = "";
+
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+
+const submitForm = async (event) => {
+    event.preventDefault();
+
+    const form = event.target;
+
+    const data = {
+        email: form.email.value,
+        password: form.password.value
+    }
+
+    const response = await fetch('/api/auth/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+
+    const result = await response.json();
+    console.log(result);
+
+    if (!result.success) {
+        alert("Email ou mot de passe incorrect");
+        return;
+    }
+
+    // On sauvegarde le token dans un cookie
+    setCookie("token", result.result.token);
+
+    // on redirige vers la page du compte
+    window.location = "/me";    
+}
 </script>
 
 <template>
     <main class="main-content">
         <div class="content-overlay">
-            <form action="/auth/login" method="POST">
-                <label for="email">Email:</label>
-                <input type="text" id="email" name="email" required>
-                
-                <label for="password">Password:</label>
-                <input type="password" id="password" name="password" required>
-                
+            <form @submit="submitForm">
+                <label>Email:</label>
+                <input type="text" name="email" required>
+
+                <label>Password:</label>
+                <input type="password" name="password" required>
+
                 <button type="submit">Login</button>
             </form>
         </div>
@@ -19,26 +63,26 @@
 
 <style scoped>
 .main-content {
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  z-index: 1;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 1;
 }
 
 .content-overlay {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  z-index: 2;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  padding-top: 10vh;
-  overflow-y: auto;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 2;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding-top: 10vh;
+    overflow-y: auto;
 }
 
 form {
