6e109110dac6b9229590cc38b5e421cb8791ad84
matmathmat <232357120+matmathmat@users.noreply.github.com>
2026-02-12 23:59:46 +0100

    Ajout model user

---
+++ src/lib/types/user.ts
@@ -0,0 +1,14 @@
+export interface MyUser {
+	id: number;
+	firstname: string;
+	lastname: string;
+	isAdmin: number;
+	email: string;
+}
+
+export interface OtherUser {
+	id: number;
+	firstname: string;
+	lastname: string;
+	isAdmin: number;
+}
