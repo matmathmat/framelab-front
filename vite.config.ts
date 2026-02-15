19e8755b21705a0321995df6b3ed6f84f1c03d62
matmathmat <232357120+matmathmat@users.noreply.github.com>
2026-02-15 22:29:07 +0100

    Ajout proxy pour lire dossier public du back

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,12 @@ export default defineConfig({
         target: 'http://localhost:3000',
         changeOrigin: true,
         secure: false
-      }
+      },
+      '/uploads': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false
+      }      
     }
   }
 });
