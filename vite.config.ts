d2af167a08400e515cdf3124bdc79874120b0fcb
matmathmat <232357120+matmathmat@users.noreply.github.com>
2026-03-25 14:47:47 +0100

    Détecte environnement pour exposer le bon proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,29 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-  plugins: [sveltekit()],
-  
-  server: {
-    allowedHosts: ['framelab-jbey.onrender.com', 'framelab-front.onrender.com'],
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false
-      },
-      '/uploads': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false
-      }      
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  const proxyTarget = mode === 'production' 
+    ? 'https://framelab-back.onrender.com/' 
+    : 'http://localhost:3000';
+
+  return {
+    plugins: [sveltekit()],
+    server: {
+      allowedHosts: ['framelab-jbey.onrender.com', 'framelab-front.onrender.com'],
+      proxy: {
+        '/api': {
+          target: proxyTarget,
+          changeOrigin: true,
+          secure: false,
+        },
+        '/uploads': {
+          target: proxyTarget,
+          changeOrigin: true,
+          secure: false
+        }
+      }
     }
-  }
+  };
 });
